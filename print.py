6930365f16805157a7856bcc1fd2a98d411c9fd0
Leon <hityixiaoyang.com>
2014-11-28 13:09:35 +0000

    1.股票查询脚本更新：格式化显示文本

---
+++ print.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+formatter = "%r %r %r %r"
+
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
--- stock.py
+++ stock.py
@@ -3,8 +3,9 @@
 
 import urllib.request
 
-debug=True
-#debug=False
+#debug=True
+debug=False
+headerFormat="%-12s %-8s %-8s %-8s %-8s %-8s %-8s"
 
 class Utility:
     def ToGB(str):
@@ -31,34 +32,23 @@ class StockInfo:
         #return like: v_s_sz000858="51~五 粮 液~000858~18.10~0.01~0.06~94583~17065~~687.07";
         return f.readline()
         f.close()
-
-    def ParseResultStr(resultstr):
+    def ParseResultStr(num,resultstr):
         if(debug): print(resultstr)
         slist=resultstr[14:-3]
         if(debug): print(slist)
         slist=slist.split('~')
 
-        if(debug) : print(slist)
-
-        #print('*******************************')
-        print('  股票名称:', slist[1])
-        print('  股票代码:', slist[2])
-
-        print('  当前价格:', slist[3])
-        print('  涨    跌:', slist[4])
-        print('  涨   跌%:', slist[5],'%')
-        print('成交量(手):', slist[6])
-        print('成交额(万):', slist[7])
-        #print('date and time is :', dateandtime)
-        print('*******************************')
+        if(debug) : print(slist)        
+        lineStr=headerFormat % (num,slist[3],slist[4],slist[5],slist[6],slist[7],slist[1])
+        print(lineStr)
 
     def GetStockInfo(num):
         str=StockInfo.GetStockStrByNum(num)
         strGB=Utility.ToGB(str)
-        StockInfo.ParseResultStr(strGB)
-
-
+        StockInfo.ParseResultStr(num,strGB)
 if __name__ == '__main__':
-    stocks = ['sz002065']
+    stocks = ['sz002065','sz002439','sz002294','sz300378','sz002577']
+    headerStr=headerFormat % ('Code','Last','Chg','Chg%','Hand','Volume','Name')
+    print(headerStr)
     for stock in stocks:
         StockInfo.GetStockInfo(stock)

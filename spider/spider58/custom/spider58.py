c51901ddb73b73b1a2c693739c7e1a006258c1db
Leon <hityixiaoyang@gmail.com>
2016-10-28 18:19:51 +0000

    1.提取具体租房信息，进行基本筛选和高亮显示

--- spider/spider58/custom/spider58.py
+++ spider/spider58/custom/spider58.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+"""
+用于监控58租房信息列表
+"""
+
 import sys
 import csv
 import os
@@ -28,7 +32,12 @@ def get_doc_byUrllib2(url):
         'Accept':'"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"'
     }
     req = urllib2.Request(url=url,headers=headers)
-    request = urllib2.urlopen(req, timeout=10)
+    try:
+        request = urllib2.urlopen(req, timeout=10)
+    except urllib2.URLError:
+        return None
+    except urllib2.HTTPError:
+        return None
     doc = request.read()
 
     # detect charset
@@ -53,33 +62,44 @@ def get_doc_byUrllib2(url):
 class Info:
     url = None
     title = None
+    price = ""
+    showroom = ""
+    place1 = ""
+    place2 = ""
+    post_time = ""
+
 
 class Parser58:
-    def __init__(self,url):
-        self.url = url
+    def __init__(self,urls):
+        self.urls = urls
         self.soup = None
         self.doc = None
         self.infos = {}
+        self.query_count = 0
 
     def parse(self):
         have_news = False
         fp = open('infos.data', 'a+')
 
-        self.doc = get_doc_byUrllib2(self.url)
-        self.soup = BeautifulSoup(self.doc, "html.parser", from_encoding="utf-8")
-        trs = self.soup.select('a.t')
+        for url in self.urls:
+            self.doc = get_doc_byUrllib2(url)
+            if not self.doc:
+                continue ;
+            self.soup = BeautifulSoup(self.doc, "html.parser", from_encoding="utf-8")
+            trs = self.soup.select('a.t')
 
-        if trs == None or len(trs) == 0:
-            return
-        for a in trs:
-            info = Info()
-            info.url = a['href']
-            info.title = a.string
-            if not self.infos.has_key(info.title):
-                self.infos[info.title] = info
-                fp.write("<a href=\"%s\">%s</a><br>\n" % (info.url, info.title))
-                have_news = True
-                print("%s" % (info.title))
+            if trs == None or len(trs) == 0:
+                return
+            for a in trs:
+                info = Info()
+                info.url = a['href']
+                info.title = a.string
+                if not self.infos.has_key(info.title):
+                    self.infos[info.title] = info
+                    fp.write("<a href=\"%s\">%s</a><br>\n" % (info.url, info.title))
+                    have_news = True
+                    print("%s" % (info.title))
+
         if have_news:
             # 2500Hz, 200ms
             os.system("beep -f 1000 -l 200")
@@ -88,18 +108,99 @@ class Parser58:
         have_news = False
         fp = open('infos.data', 'a+')
 
-        print("--------------------------------------------")
-        self.doc = get_doc_byUrllib2(self.url)
-        self.soup = BeautifulSoup(self.doc, "html.parser", from_encoding="utf-8")
-        trs = self.soup.find('tr')
+        self.query_count += 1
+        for url in self.urls:
+            self.doc = get_doc_byUrllib2(url)
+            self.soup = BeautifulSoup(self.doc, "html.parser", from_encoding="utf-8")
+            trs = self.soup.find_all('tr')
+            if not trs or len(trs) == 0:
+                return
+            for tr in trs:
+                if tr.has_attr("logr"):
+                    alinks = tr.find_all('a',class_='t')
+                    if alinks == None or len(alinks) == 0:
+                        continue
+                    a = alinks[0]
 
-        if trs == None or len(trs) == 0:
-            return
-        for tr in trs:
-            print(tr)
-            ass = tr.find("a")
-            for a in tr:
-                print(a)
+                    info = Info()
+
+                    # 提取标题，链接
+                    info.url = a['href']
+                    info.title = a.string
+
+                    if not self.infos.has_key(info.title):
+                        price_isLow = False
+                        delicate = info.title.find('精')!=-1
+                        excepted_rome = False
+                        # 忽略置顶条目
+                        ding = tr.find("span",class_="ico ding")
+                        if ding:
+                            continue
+
+                        # 提取价格，厅室，位置
+                        showroom = tr.find("span",class_='showroom')
+                        if showroom:
+                            info.showroom = showroom.string
+                            if len(info.showroom) < 6:
+                                info.showroom = info.showroom + "0卫"
+
+                        excepted_rome = info.showroom.find("1室1厅") != -1
+                        price = tr.find("b", class_='pri')
+                        if price:
+                            info.price = price.string
+                            price_isLow = (int(info.price) <= 2000)
+
+                        place1 = tr.find("a", class_='a_xq1')
+                        place2 = tr.find("span", class_='f12')
+                        if place1:
+                            info.place1 = place1.string or ""
+                        if place2:
+                            info.place2 = place2.string or ""
+                        info.place1.strip()
+                        info.place2.strip()
+
+                        post_time = tr.find("p",class_="qj-renaddr")
+                        if post_time:
+                            post_time = post_time.get_text()
+                            if post_time:
+                                post_time = post_time.strip()
+                                post_time = post_time.replace(' ', '')
+                                post_time = post_time.replace('\r', '')
+                                post_time = post_time.replace('\n', '')
+
+                                post_times = post_time.split('/')
+                                if len(post_times) >= 2:
+                                    info.post_time = post_times[1]
+
+                        self.infos[info.title] = info
+                        if self.query_count > 0:
+                            style = ""
+                            if excepted_rome:
+                                style = "recommend-normal"
+                                if price_isLow:
+                                    style = "recommend-better"
+                                    if delicate:
+                                        style = "recommend-best"
+                                else:
+                                    if delicate:
+                                        style = "recommend-better"
+                            fp.write("<p class=\"pitem %s\">"
+                                     "<span class=\"room %s %s\">%s</span>/"
+                                     "<span class=\"item price %s\">%s</span>/"
+                                     "<span class=\"item post_time\">%s</span>/"
+                                     "<span class=\"item place\">%s-%s</span>/"
+                                     "<a target=\"_blank\"href=\"%s\">%s</a></p>\n"
+                                     % (style ,
+                                        "excepted_rome" if (excepted_rome) else "unexcepted_rome",
+                                        "delicate" if (delicate) else "",
+                                        info.showroom,
+                                        "low_price" if price_isLow else "",
+                                        info.price, info.post_time, info.place1,info.place2,info.url, info.title))
+                        have_news = True
+                        print("%-8s %-8s %-8s %-s\t %s" % (info.price, info.showroom, info.post_time, info.place1+"/"+info.place2, info.title))
+        if have_news:
+            # 2500Hz, 200ms
+            os.system("beep -f 1000 -l 200")
         fp.close()
 
 """
@@ -114,12 +215,17 @@ if __name__ == "__main__":
     sys.setdefaultencoding('utf-8')
 
     fp = open('infos.data', 'w')
-    fp.write("<head><meta charset=UTF-8> <meta name=renderer content=webkit></head>")
+    fp.write("<head><meta charset=UTF-8> <meta name=renderer content=webkit><link rel=\"stylesheet\" type=\"text/css\" href=\"./style.css\"/></head>")
     fp.close()
 
-    parser = Parser58("http://sz.58.com/nanshan/zufang/0/j1/?minprice=0_2500&PGTID=0d300008-0071-3f5c-78fc-4306be39860a&ClickID=6")
+    #parser = Parser58(["http://sz.58.com/nanshan/zufang/0/j1/?minprice=0_2500&PGTID=0d300008-0071-3f5c-78fc-4306be39860a&ClickID=6",
+    #"http://sz.58.com/baoanzhongxinquba/zufang/0/j1/?minprice=0_2500&PGTID=0d300008-0180-96ef-9d87-e8e13a507831&ClickID=3"
+    #])
+
+    parser = Parser58(
+        ["http://sz.58.com/nanshan/zufang/0/j1/?minprice=0_2500&PGTID=0d300008-0071-3f5c-78fc-4306be39860a&ClickID=6"])
 
     while True:
-        parser.parse()
-        sleep(10)
+        parser.parse2()
+        sleep(30)
     pass
